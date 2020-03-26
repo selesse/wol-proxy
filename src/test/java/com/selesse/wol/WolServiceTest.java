afb678898db5f15f8ee37b916b57183ca207e108
Alex Selesse <alex@selesse.com>
2020-03-26 19:26:11 -0400

    Make test likelier to work

--- src/test/java/com/selesse/wol/WolServiceTest.java
+++ src/test/java/com/selesse/wol/WolServiceTest.java
@@ -3,13 +3,34 @@ package com.selesse.wol;
 import com.selesse.wol.packet.WolPacket;
 import org.junit.Test;
 
+import java.net.InetAddress;
 import java.net.NetworkInterface;
 import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.util.Collections;
+import java.util.Optional;
 
 public class WolServiceTest {
 
     @Test
-    public void send() throws SocketException {
-        new WolService(new WolPacket("aa:aa:aa:aa:aa:ab")).sendWakeOnLan(NetworkInterface.getByName("eth0"));
+    public void sendingAPacketWorks() throws SocketException {
+        WolPacket wolPacket = new WolPacket("aa:aa:aa:aa:aa:ab");
+        WolService wolService = new WolService(wolPacket);
+        NetworkInterface networkInterface =
+                Optional.ofNullable(NetworkInterface.getByName("eth0"))
+                        .orElse(defaultNetworkInterface());
+        wolService.sendWakeOnLan(networkInterface);
+    }
+
+    private NetworkInterface defaultNetworkInterface() throws SocketException {
+        return Collections.list(NetworkInterface.getNetworkInterfaces()).
+                stream().
+                filter(x -> {
+                    try {
+                        return Collections.list(x.getInetAddresses()).contains(InetAddress.getLocalHost());
+                    } catch (UnknownHostException e) {
+                        return false;
+                    }
+                }).findFirst().orElseThrow(() -> new RuntimeException("Could not find any network interfaces"));
     }
 }
