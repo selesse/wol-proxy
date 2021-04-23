29809ba2ca076d6924e5afc530777b1260a9db35
Alex Selesse <alex@selesse.com>
2021-04-23 13:10:17 -0400

    Update MAC address for new computer

--- src/main/java/com/selesse/wol/DesktopWakeOnLanService.java
+++ src/main/java/com/selesse/wol/DesktopWakeOnLanService.java
@@ -7,7 +7,7 @@ import java.net.SocketException;
 
 public class DesktopWakeOnLanService {
     public void sendWakeOnLan() throws SocketException {
-        WolService wolService = new WolService(new WolPacket("aa:aa:aa:aa:aa:ac"));
+        WolService wolService = new WolService(new WolPacket("2c:f0:5d:70:31:1a"));
         wolService.sendWakeOnLan(NetworkInterface.getByName("eth0"));
     }
 }
